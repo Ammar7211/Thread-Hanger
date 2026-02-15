46cdef099971c01b7d30fce0f7085aa3494fe8d9
Ammar7211 <ammar.yaqoob721@gmail.com>
2026-02-15 19:50:47 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Only the name of your repo, surrounded by slashes
-  base: '/Thread-Hanger/', 
+  // Change this from '/Thread-Hanger/' to just '/'
+  base: '/', 
   plugins: [react()],
 })
